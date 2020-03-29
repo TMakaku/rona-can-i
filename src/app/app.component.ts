7cdafc57c3f1e0b5e8047c1aa284b9509bd3e4dc
Tarsi Maria <tmaria@Tarsis-MacBook-Pro.local>
2020-03-29 12:45:42 +0200

    parse url index quick and dirty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { S1LottieConfig } from '@sentinel-one/s1-lottie';
 import { AnimationItem } from 'lottie-web';
 import { AfterViewInit } from '@angular/core';
 import { ElementRef } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+const EVENT_RESIZE = 'resize';
 
 @Component({
   selector: 'app-root',
@@ -11,31 +15,41 @@ import { ElementRef } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  canIText: string;
-  loading = false;
-  myAnimation: AnimationItem;
+  private myAnimation: AnimationItem;
+  private windowsize;
   private prevIndex = 0;
 
-  titles: string[] = [
+  private titles: string[] = [
     "Im hungry.. can i leave the house?",
     "I want icecream.. can i leave the house?",
     "My roommate farted.. can i leave the house?",
     "I see a doggo outside.. can i leave the house?"
   ];
 
+  canIText: string;
   lottieParams: S1LottieConfig = {
     path: 'assets/Refresh.json',
-    renderer: 'canvas',
+    renderer: 'svg',
     loop: false,
     autoplay: false
   };
+  lottieSize = 250;
 
-  constructor(private elementRef: ElementRef){
-
+  constructor(private elementRef: ElementRef ) {
+    this.windowsize = fromEvent(document.defaultView, EVENT_RESIZE)
+    .pipe(debounceTime(100));
   }
+
   ngOnInit() {
-    this.determineText();
+    this.lottieSize = this.isMobileDevice() ? 150 : 250;
+    this.windowsize.subscribe(() =>  {
+      const {innerHeight, innerWidth} = document.defaultView;
+      console.log(innerHeight, innerWidth);
+    })
+    const urlIndex = Number(window.location.pathname.substring(1));
+    this.determineText(urlIndex);
   }
+
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'green';
   }
@@ -52,14 +66,20 @@ export class AppComponent implements OnInit, AfterViewInit {
     window.setTimeout(() => this.determineText(), 350);
   }
 
-  private determineText() {
-    const randomIndex = this.getRandomIndex();
+  private determineText(urlIndex?) {
+    const randomIndex = isNaN( urlIndex ) || !urlIndex ? this.getRandomIndex(): urlIndex;
     this.prevIndex = randomIndex
+    window.history.pushState('', "Rona can I?", `/` + randomIndex);
     this.canIText = this.titles[randomIndex];
   }
 
   private getRandomIndex() {
     const index = Math.floor(Math.random() * this.titles.length);
-    return index !== this.prevIndex ? index : this.getRandomIndex();
+    return index !== NaN && index !== this.prevIndex ? index : this.getRandomIndex();
   }
+
+  isMobileDevice() {
+    // todo replace with this.windowSize..
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  };
 }
